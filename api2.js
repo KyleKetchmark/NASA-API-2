4941f24de027408cb6990afecd27b12df7dbca43
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2021-11-19 10:33:50 -0800

    NASA API-2 complete

--- api2.js
+++ api2.js
@@ -1,4 +1,53 @@
-// let api = api.nasa.gov/;
-let apiKey = cI8Qfwuk3mwfRbgjudoTvMoC1BhlU5aQBUEUaXmq;
+let baseUrl = 'https://api.nasa.gov/planetary/apod?'
+let apiKey = 'cI8Qfwuk3mwfRbgjudoTvMoC1BhlU5aQBUEUaXmq';
+let url;
+//variables
+const searchForm = document.querySelector('search-form');
+const searchBtn = document.getElementById('submit');
+const title = document.getElementById('title');
+const summary = document.getElementById('summary');
+const imageWrapper = document.getElementById('image');
+
+
+//event listeners
+searchBtn.addEventListener('click', fetchApi);
+
 
 //Fetch Fx
+async function fetchApi(e) {
+    // e.preventDefault(e);
+    // let earthDate = String(date.value);
+    url = `${baseUrl}api_key=${apiKey}`
+    
+    let result = await fetch(url);
+    let json = await result.json();
+    displayContent(json);
+}
+
+fetchApi();
+
+//Display content fx
+function displayContent(json) {
+    console.log("Nasa Data:", json);
+    
+    let titleResults = json.title;
+    let sumResults = json.explanation;
+    let imgResults = json.hdurl;
+    
+    
+    let titleElement = document.createElement('h2');
+    titleElement.innerHTML = titleResults;
+
+    let paraElement = document.createElement('p');
+    paraElement.innerText = sumResults;
+
+    let newImgElement = document.createElement('img');
+    newImgElement.src = imgResults;
+    // newImgElem2.class = "d-block w-100"
+
+    title.appendChild(titleElement);
+    summary.appendChild(paraElement);
+    imageWrapper.appendChild(newImgElement);
+    
+
+}
