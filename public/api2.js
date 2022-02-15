6b60453b8ea42891dfc4440f9a673958716428c4
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2022-02-15 11:37:41 -0800

    video inc w/ styling changes

--- public/api2.js
+++ public/api2.js
@@ -2,52 +2,95 @@ let baseUrl = 'https://api.nasa.gov/planetary/apod?'
 let apiKey = 'cI8Qfwuk3mwfRbgjudoTvMoC1BhlU5aQBUEUaXmq';
 let url;
 //variables
-const searchForm = document.querySelector('search-form');
+const cardWrap = document.getElementById('card-wrapper');
+const searchForm = document.querySelector('form');
 const searchBtn = document.getElementById('submit');
 const title = document.getElementById('title');
 const summary = document.getElementById('summary');
 const imageWrapper = document.getElementById('image');
-
-
+const getDate = document.getElementById('get-date');
+let switcher = false;
 //event listeners
-searchBtn.addEventListener('click', fetchApi);
-
+searchForm.addEventListener('submit', fetchApi);
+searchBtn.addEventListener('click', viewer)
 
 //Fetch Fx
 async function fetchApi(e) {
-    // e.preventDefault(e);
-    // let earthDate = String(date.value);
-    url = `${baseUrl}api_key=${apiKey}`
+    e.preventDefault();
+    
+    let date = getDate.value;
+    console.log(date);
+    url = `${baseUrl}api_key=${apiKey}&date=${date}`
     
     let result = await fetch(url);
     let json = await result.json();
-    displayContent(json);
+    if(viewer) {
+        displayContent(json);
+    } else {
+        null
+    }
 }
 
-fetchApi();
+
+function viewer() {
+    cardWrap.style.display = 'flex';
+    cardWrap.style.flexDirection = 'column';
+    cardWrap.style.alignItems = 'center';
+    cardWrap.style.paddingTop = '10vh';
+    cardWrap.style.height = '100%';
+    cardWrap.style.width = '100%';
+}
 
 //Display content fx
 function displayContent(json) {
     console.log("Nasa Data:", json);
-    
+    let newImgElement = document.createElement('img');
+    let newVidElem = document.createElement('iframe');
+    let newT = document.createElement('h5');
+    let newAuth = document.createElement('p');
+    let newP = document.createElement('p')
+
+    while (imageWrapper.firstChild) {
+        imageWrapper.removeChild(imageWrapper.firstChild);
+    }
+    while (title.firstChild) {
+        title.removeChild(title.firstChild);
+        // title.removeChild(newAuth.firstChild);
+    }
+    while (summary.firstChild) {        
+        summary.removeChild(summary.firstChild);
+    }
+    // let card = document.createElement('div')
+    //returned object API vars
     let titleResults = json.title;
     let sumResults = json.explanation;
     let imgResults = json.hdurl;
-    
-    
-    let titleElement = document.createElement('h2');
-    titleElement.innerHTML = titleResults;
+    let vidResults = json.url;
+    let author = json.copyright;
+    //img
+    if(imgResults === undefined) {
+        newVidElem.src = vidResults;
+        newVidElem.type = 'video';
+        imageWrapper.appendChild(newVidElem);
+    } else {
+        newImgElement.src = imgResults;
+        imageWrapper.appendChild(newImgElement);
+    }
+    //title
+    newT.innerText = titleResults;
+    title.appendChild(newT);
 
-    let paraElement = document.createElement('p');
-    paraElement.innerText = sumResults;
+    // author
+    newAuth.id = 'author';
+    //if no author display unknown
+    if(author === undefined) {
+        newAuth.innerText = `Created By Unknown`;    
+    } else {
+        newAuth.innerText = `Created By ${author}`;
+    }
+    title.appendChild(newAuth);
 
-    let newImgElement = document.createElement('img');
-    newImgElement.src = imgResults;
-    // newImgElem2.class = "d-block w-100"
-
-    title.appendChild(titleElement);
-    summary.appendChild(paraElement);
-    imageWrapper.appendChild(newImgElement);
-    
-
+    //summary
+    newP.innerText = sumResults;
+    summary.appendChild(newP);    
 }
